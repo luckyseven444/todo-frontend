b3538c2a068e14428ed0b44ee2bd31598cf48015
Gazi Salah Uddin <gazisalahuddin73@gmail.com>
2024-12-08 15:36:49 +0600

    todo table redesinged

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -275,165 +275,175 @@ const handleStatusChange = async (id) => {
   }
 }
 </script>
+
 <template>
-  <div class="container-fluid">
-    <div class="row">
+  <div class="container">
+    <div class="row mb-3 p-2">
       <!-- New todo create form -->
-      <form class="row needs-validation" ref="formRefCreateForm" @submit="handleTodoCreateForm">
-        <div class="row justify-content-end">
-          <div class="col">
-            <input
-              v-model="title"
-              type="text"
-              class="form-control"
-              :class="{
-                'is-valid': createFormValid,
-                'is-invalid': !createFormValid,
-              }"
-              id="validationCustom03"
-              required
-            />
-            <div class="invalid-feedback" v-if="newTodoCreateErrorMessage">
-              {{ newTodoCreateErrorMessage }}
-            </div>
-          </div>
-          <div class="col">
-            <button class="btn btn-primary btn-sm" type="submit">Add</button>
-          </div>
-        </div>
-      </form>
-    </div>
-    <div class="row">
-      <table class="table">
-        <thead>
-          <th>id</th>
-          <th>Title</th>
-          <th>Status</th>
-          <th>Actions</th>
-        </thead>
-        <tbody>
-          <tr v-for="todo in todos" :key="todo.id">
-            <td>{{ todo.id }}</td>
-            <td>{{ todo.title }}</td>
-            <td>
-              <div class="form-check">
-                <input
-                  class="form-check-input"
-                  type="checkbox"
-                  value=""
-                  id="flexCheckChecked"
-                  :checked="todo.status === 1"
-                  @click="handleStatusChange(todo.id)"
-                />
-              </div>
-            </td>
-            <td>
-              <button
-                type="button"
-                data-bs-toggle="modal"
-                data-bs-target="#todoInfoEditModal"
-                data-bs-backdrop="false"
-                class="btn btn-primary btn-sm me-1"
-                @click="saveInfoToReactiveVariablesForEdit(todo.id, todo.title)"
-              >
-                Edit
-              </button>
-
-              <button
-                type="button"
-                data-bs-toggle="modal"
-                data-bs-target="#todoDeleteModal"
-                class="btn btn-danger btn-sm me-1"
-                @click="saveInfoToReactiveVariablesForDelete(todo.id)"
-              >
-                Delete
-              </button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
-
-  <!-- Modal for data edit -->
-  <div
-    class="modal fade"
-    id="todoInfoEditModal"
-    tabindex="-1"
-    aria-labelledby="todoInfoEditModalLabel"
-    aria-hidden="true"
-  >
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="todoInfoEditModalLabel">Edit customer</h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
-        </div>
-
-        <div class="modal-body">
-          <form
-            class="row g-3 needs-validation"
-            novalidate
-            ref="formRefEdit"
-            @submit="validateFormAndSendToServerEditInfo"
-          >
-            <div class="col-md-6">
-              <label for="name" class="form-label">Title</label>
+      <div class="col text-end">
+        <form
+          class="needs-validation d-inline-block"
+          ref="formRefCreateForm"
+          @submit="handleTodoCreateForm"
+        >
+          <div class="row g-2 align-items-center">
+            <div class="col-auto">
               <input
+                v-model="title"
                 type="text"
                 class="form-control"
-                id="name"
-                v-model="selectedTodoTitle"
+                :class="{
+                  'is-valid': createFormValid,
+                  'is-invalid': !createFormValid,
+                }"
+                id="validationCustom03"
                 required
+                placeholder="Add todo"
               />
-              <div class="invalid-feedback">Please enter title</div>
+              <div class="invalid-feedback" v-if="newTodoCreateErrorMessage">
+                {{ newTodoCreateErrorMessage }}
+              </div>
             </div>
+            <div class="col-auto">
+              <button class="btn btn-primary" type="submit">Add</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
 
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="submit" class="btn btn-primary">Save changes</button>
-            </div>
-          </form>
+    <div class="row">
+      <div class="col">
+        
+        <table class="table table-striped table-bordered table-hover w-100">
+          <thead class="table-light">
+            <tr>
+              <th>Title</th>
+              <th>Status</th>
+              <th class="text-center">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="todo in todos" :key="todo.id">
+              <td>{{ todo.title }}</td>
+              <td class="text-center">
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    :checked="todo.status === 1"
+                    @click="handleStatusChange(todo.id)"
+                  />
+                </div>
+              </td>
+              <td class="text-center">
+                <button
+                  type="button"
+                  class="btn btn-primary btn-sm me-1"
+                  data-bs-toggle="modal"
+                  data-bs-target="#todoInfoEditModal"
+                  @click="saveInfoToReactiveVariablesForEdit(todo.id, todo.title)"
+                >
+                  Edit
+                </button>
+                <button
+                  type="button"
+                  class="btn btn-danger btn-sm"
+                  data-bs-toggle="modal"
+                  data-bs-target="#todoDeleteModal"
+                  @click="saveInfoToReactiveVariablesForDelete(todo.id)"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <!-- Modal for data edit -->
+    <div
+      class="modal fade"
+      id="todoInfoEditModal"
+      tabindex="-1"
+      aria-labelledby="todoInfoEditModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="todoInfoEditModalLabel">Edit customer</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+
+          <div class="modal-body">
+            <form
+              class="row g-3 needs-validation"
+              novalidate
+              ref="formRefEdit"
+              @submit="validateFormAndSendToServerEditInfo"
+            >
+              <div class="col-md-6">
+                <label for="name" class="form-label">Title</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="name"
+                  v-model="selectedTodoTitle"
+                  required
+                />
+                <div class="invalid-feedback">Please enter title</div>
+              </div>
+
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                  Close
+                </button>
+                <button type="submit" class="btn btn-primary">Save changes</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
-  </div>
 
-  <!-- Modal for customer delete -->
-  <div
-    class="modal fade"
-    id="todoDeleteModal"
-    tabindex="-1"
-    aria-labelledby="todoDeleteModalLabel"
-    aria-hidden="true"
-  >
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="todoDeleteModalLabel">Delete todo</h5>
-          <button
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
-        </div>
+    <!-- Modal for customer delete -->
+    <div
+      class="modal fade"
+      id="todoDeleteModal"
+      tabindex="-1"
+      aria-labelledby="todoDeleteModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="todoDeleteModalLabel">Delete todo</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
 
-        <div class="modal-body">
-          <form class="row g-3 needs-validation" @submit="deleteTodo">
-            <label for="name" class="form-label">Are want to delete?</label>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-                Cancel
-              </button>
-              <button type="submit" class="btn btn-danger">Yes</button>
-            </div>
-          </form>
+          <div class="modal-body">
+            <form class="row g-3 needs-validation" @submit="deleteTodo">
+              <label for="name" class="form-label">Are want to delete?</label>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                  Cancel
+                </button>
+                <button type="submit" class="btn btn-danger">Yes</button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
